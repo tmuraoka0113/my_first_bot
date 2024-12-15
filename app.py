b3b8914c40a15192d72b51a70ca2082123306b37
tmuraoka0113 <taihei1324@gmail.com>
2024-12-15 15:22:33 +0900

    first commit

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=event.message.text))
+    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=event.message.text + "welcome to TDL"))
 
 
 if __name__ == "__main__":
